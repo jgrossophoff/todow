762742ca1da48262ed8854135d28813a48157d1a
Jens Groß Ophoff <jens@grossophoff.de>
2016-06-10 11:40:20 +0200

    web ui: add viewport meta tag.

--- cmd/todow-server/main.go
+++ cmd/todow-server/main.go
@@ -346,6 +346,7 @@ var tmpl = template.Must(template.New("").Parse(`
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Todow</title>
 	<style>
 		td {
